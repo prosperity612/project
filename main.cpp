712990e107699d91463f7d73a48b3d530e7108f0
prosperity <prospetity6122024@gmail.com>
2026-06-30 20:37:50 +0300

    Real day 1: Add Unit, Player and Enemy

--- main.cpp
+++ main.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+#include <string>
+
+class Unit {
+protected:
+    std::string name;
+    int damage;
+    int health;
+public:
+    Unit(std::string n, int d, int h)
+        : name(n), damage(d), health(h) {};
+
+    std::string getName() const{
+        return name;
+    }
+
+    int getDamage() const{
+        return damage;
+    }
+
+    int getHealth() const{
+        return health;
+    }
+
+    //setters!
+
+
+    void takeDamage(int dmg) {
+        health -= dmg;
+    }
+
+    virtual void attack(Unit& target) {
+        target.takeDamage(damage);
+    }
+
+    bool isDead() const{
+        return health <= 0;
+    }
+};
+
+class Player : public Unit {
+// add private/protected
+public:
+    Player(std::string n, int d, int h)
+       : Unit(n, d, h) {};
+};
+
+class Enemy : public Unit {
+// add private/protected
+public:
+    Enemy(std::string n, int d, int h)
+          : Unit(n, d, h) {};
+};
 
 int main() {
     std::cout << "Boss fight\n";
+
 
     return 0;
 }
